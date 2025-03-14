950d63bd4b36150817e9b609bc2acc1332ec7ff7
Silas Joisten <silasjoisten@proton.me>
2025-03-14 16:05:59 +0200

    Fix

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 /**
  * This file is part of Storyblok PHP Tiptap Extension.
  *
- * (c) SensioLabs Deutschland <info@sensiolabs.de>
+ * (c) Storyblok GmbH
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -18,7 +18,7 @@ use Ergebnis\PhpCsFixer\Config\RuleSet\Php83;
 $header = <<<'HEADER'
 This file is part of Storyblok PHP Tiptap Extension.
 
-(c) SensioLabs Deutschland <info@sensiolabs.de>
+(c) Storyblok GmbH
 
 For the full copyright and license information, please view the LICENSE
 file that was distributed with this source code.
